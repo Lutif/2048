e830d9ffea12a0da68dc7c7f42cddcba2d1f160d
lutif <lutif.mandhro@gmail.com>
2023-12-02 13:33:03 +0500

    fixes

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -7,7 +7,7 @@ const tileStyle = {
   textAlign: "center",
   lineHeight: "10vw",
   justifyContent: "center",
-  fontSize: "6vw",
+  fontSize: "4vw",
   color: "white",
   fontWeight: "bold",
   borderRadius: "10px",
@@ -19,8 +19,8 @@ const getTileColor = (value) => {
   if (!value) return '#fff';
   const baseLog = Math.log2(value);
   const red = Math.min(255, baseLog * 5).toString(16).padStart(2, '0');
-  const green = Math.min(255, 255 - baseLog * 20).toString(16).padStart(2, '0');
-  const blue = Math.min(255, 255 - baseLog * 25).toString(16).padStart(2, '0');
+  const green = Math.max(0, 255 - baseLog * 20).toString(16).padStart(2, '0');
+  const blue = Math.max(0, 255 - baseLog * 25).toString(16).padStart(2, '0');
   return `#${red}${green}${blue}`;
 }
 export default function Tile({value}) {
